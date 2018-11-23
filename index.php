eb1a4e66d5f0b32406269a8234a596a5d13e1da1
Javier Corona Moreno <jackshel_10@hotmail.com>
2018-11-23 14:42:53 -0600

    Integracion Funcionalidad
    Integracion Funcionalidad

---
+++ index.php
@@ -0,0 +1,128 @@
+<!--
+Theme: Esta También Es Tu Pepsi
+Version: 2
+Author: Oet Capital
+4 de Noviembre 2018
+-->
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <link rel="prefetch" href="ui/img/logo-pepsi-80.svg">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="google" content="notranslate">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0000, minimum-scale=1.0000, maximum-scale=1.0000, user-scalable=no">
+    <title>Esta También Es Tu Pepsi | Pepsi México</title>
+    <meta name="description" content="El lugar perfecto para encontrar las mejores promociones de toda la línea de Pepsi">
+    <link rel="canonical" href="www.estatambienestupepsi.com">
+    <style>
+      #loading{position:fixed;top:0;left:0;width:100vw;height:100vh;-webkit-flex-direction:column;-moz-flex-direction:column;-ms-flex-direction:column;-o-flex-direction:column;flex-direction:column;-webkit-align-items:center;-moz-align-items:center;-ms-align-items:center;-o-align-items:center;align-items:center;-webkit-justify-content:center;-moz-justify-content:center;-ms-justify-content:center;-o-justify-content:center;justify-content:center;z-index:100;background:#fff}#loading>div>img{opacity:0;margin:0 auto 20px;-webkit-animation:fadeIn .7s ease-in .7s forwards;-moz-animation:fadeIn .7s ease-in .7s forwards;-ms-animation:fadeIn .7s ease-in .7s forwards;-o-animation:fadeIn .7s ease-in .7s forwards;animation:fadeIn .7s ease-in .7s forwards}#loading>div>.loader{opacity:0;position:relative;margin:0 auto;width:56px;height:20px;display:-webkit-box;display:-ms-flexbox;display:-webkit-flex;display:flex;-webkit-justify-content:space-between;-moz-justify-content:space-between;-ms-justify-content:space-between;-o-justify-content:space-between;justify-content:space-between;-webkit-animation:fadeIn .7s ease-in 1.2s forwards;-moz-animation:fadeIn .7s ease-in 1.2s forwards;-ms-animation:fadeIn .7s ease-in 1.2s forwards;-o-animation:fadeIn .7s ease-in 1.2s forwards;animation:fadeIn .7s ease-in 1.2s forwards}#loading>div>.loader>span{display:block;width:4px;background:#184895;height:100%;-webkit-animation:loaderSpan 1.5s infinite;-moz-animation:loaderSpan 1.5s infinite;-ms-animation:loaderSpan 1.5s infinite;-o-animation:loaderSpan 1.5s infinite;animation:loaderSpan 1.5s infinite}#loading>div>.loader>span:first-child{-webkit-animation-delay:1s;-moz-animation-delay:1s;-ms-animation-delay:1s;-o-animation-delay:1s;animation-delay:1s}#loading>div>.loader>span:nth-child(2){-webkit-animation-delay:.8s;-moz-animation-delay:.8s;-ms-animation-delay:.8s;-o-animation-delay:.8s;animation-delay:.8s}#loading>div>.loader>span:nth-child(3){-webkit-animation-delay:.6s;-moz-animation-delay:.6s;-ms-animation-delay:.6s;-o-animation-delay:.6s;animation-delay:.6s}#loading>div>.loader>span:nth-child(4){-webkit-animation-delay:.4s;-moz-animation-delay:.4s;-ms-animation-delay:.4s;-o-animation-delay:.4s;animation-delay:.4s}#loading>div>.loader>span:last-child{-webkit-animation-delay:.2s;-moz-animation-delay:.2s;-ms-animation-delay:.2s;-o-animation-delay:.2s;animation-delay:.2s}
+    </style>
+    <!-- INSERTAR AQUI -->
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="ui/fav/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="ui/fav/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="ui/fav/apple-touch-icon-76x76.png" />
+    <link rel="icon" type="image/png" href="ui/fav/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="ui/fav/favicon-16x16.png" sizes="16x16" />
+    <meta name="keywords" content="Pepsi, Pepsi Legacy, Esta También Es Tu Pepsi, Promoción, OXXO">
+    <meta property="og:title" content="Esta También Es Tu Pepsi | Pepsi México">
+    <meta property="og:url" content="www.estatambienestupepsi.com">
+    <meta property="og:site_name" content="Esta También Es Tu Pepsi | Pepsi México">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="El lugar perfecto para encontrar las mejores promociones de toda la línea de Pepsi"> <!-- FALTA -->
+    <meta property="og:image" content="ui/img/social.jpg">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@pepsimex">
+    <meta name="twitter:title" content="Esta También Es Tu Pepsi | Pepsi México">
+    <meta name="twitter:description" content="El lugar perfecto para encontrar las mejores promociones de toda la línea de Pepsi"> <!-- FALTA -->
+    <meta name="twitter:image" content="ui/img/social.jpg">
+    <link rel="stylesheet" href="ui/css/master.min.css">
+    <script src="https://code.jquery.com/jquery-latest.min.js" defer></script>
+  </head>
+  <body>
+    <div id="loading" class="standarWindow trans5 flexDisplay">
+      <div>
+        <img src="ui/img/logo-pepsi-80.svg" alt="Logotipo Pepsi" width="160" height="160">
+        <div class="loader flexDisplay rot180"> <span></span> <span></span> <span></span> <span></span> <span></span> </div>
+        <div class="loader flexDisplay mirror"> <span></span> <span></span> <span></span> <span></span> <span></span> </div>
+      </div>
+    </div>
+    <div id="scratch" class="standarWindow">
+      <div id="left_top"></div>
+      <div id="right_bottom"></div>
+      <div id="center">
+      </div>
+    </div>
+    <nav id="nav" class="flexDisplay trans5">
+      <a href="#"><img src="ui/img/logo-pepsi-80.svg"></a>
+      <img src="ui/img/oxxo_logotipo.png">
+    </nav>
+    <div id="main">
+      <div id="producto" class="flexDisplay trans5">
+        <div id="botella"></div>
+        <div id="info" class="flexDisplay">
+          <div id="obten" class="flexDisplay trans5">
+            <div id="obtenInfo" class="flexDisplay trans5">
+              <h3>Precio Especial</h3>
+              <img src="ui/img/precio.svg">
+            </div>
+            <div id="generandoInfo" class="dislplayNone">
+              <h4>Esta Es La Pepsi <br> De Cada Generación</h4>
+              <p>Generando Cupón</p>
+            </div>
+            <div id="obtenCupon" class="buttonPepsi trans5"></div>
+            <div id="campanaMovil">
+              <h5>Esta También Es Tu Pepsi <img src="ui/img/logo-pepsi-80.svg" width="13"></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="cuponwrap" class="dislplayNone">
+        <div id="cupon1"></div>
+        <a id="download" role="button" class="buttonGuardado trans5">
+          <p>Guarda Tu Cupón</p>
+        </a>
+      </div>
+      <div id="textura">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 563 616">
+      <path class="path none" d="M390,164a22,22,0,1,1-22-22,22,22,0,0,1,22,22"/>
+      <path class="path none" d="M67,136a17,17,0,1,1-17-17,17,17,0,0,1,17,17"/>
+      <path class="path none" d="M112,565a21,21,0,1,1-21-21,21,21,0,0,1,21,21"/>
+      <path class="path none" d="M300.7,531.68S309.87,557.77,334,551s16-35.63,16-35.63"/>
+      <path class="path none" d="M57.11,232.68C55.79,245.42,67.8,243.21,65.73,251,63,261.28,52.31,252.52,51,261.72s9.95,5.58,9.31,14.78-12.48,5.32-14.06,11.42c-2.25,8.74,8.87,9.06,7.9,16.2-.67,5-4.53,3.6-8.44,5.72-3.46,1.88-6.31,7.83-6.31,7.83"/>
+      </svg>
+      </div>
+    </div>
+    <div id="mensaje" class="dislplayNone">
+      <img src="ui/img/logo-pepsi-80.svg" width="140">
+      <p>¡TU CUPÓN DE <br>
+      PEPSI HA SIDO <br>
+      GUARDADO <br>
+      EXITOSAMENTE!</p>
+      <div id="social">
+        <a href="#" target="_blank"><img src="ui/img/social/fb.svg" width="40"></a>
+        <a href="#" target="_blank"><img src="ui/img/social/ig.svg" width="40"></a>
+        <a href="#" target="_blank"><img src="ui/img/social/tw.svg" width="40"></a>
+        <a href="#" target="_blank"><img src="ui/img/social/yt.svg" width="40"></a>
+      </div>
+    </div>
+    <footer id="foot" class="flexDisplay trans5">
+      <h5>Esta También Es Tu Pepsi <img src="ui/img/logo-pepsi-80.svg" width="13"></h5>
+      <h6 id="haz">Haz Ejercicio</h6>
+      <h6><span id="hazmob">Haz Ejercicio.</span> <a href="#">Consulta bases términos y condiciones.</a> <span id="marca">® Marca Registrada</span></h6>
+    </footer>
+    <!-- PREVENT LANDSCAPE -->
+    <div id="preventLandscape" class="dislplayNone">
+      <img src="ui/img/rotate.svg" width="50" height="50">
+      <p>Por favor gira tu teléfono</p>
+    </div>
+    <!-- PREVENT MINIMUM HEIGHT -->
+    <div id="preventHeight" class="dislplayNone">
+      <img src="ui/img/height.svg" width="50" height="50">
+      <p>Por favor incrementa el tamaño de tu pantalla</p>
+    </div>
+  </body>
+  <script src="ui/js/lottie.min.js" charset="utf-8"></script>
+  <script src="ui/js/front.js" defer></script>
+</html>
